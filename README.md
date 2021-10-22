f9183553b07a913462ae3af7cdcd32234b66832d
AngaBlue <contact@anga.blue>
2021-10-22 20:01:47 +1100

    Fixed index.js

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 <div align="center">
-    <h1>🧑🏻‍💻 AngaBlue Eslint</h1>
+    <h1>🧑🏻‍💻 AngaBlue Eslint Config</h1>
     <p>Eslint config package for TypeScript & JavaScript, supporting React apps too.</p>
 </div>
--- index.js
+++ index.js
@@ -34,7 +34,9 @@ module.exports = {
         indent: ['error', 4, { 'SwitchCase': 1 }],
         'no-plusplus': 'off',
         'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.tsx', '.ts'] }],
-        'import/extensions': 'off'
+        'import/extensions': 'off',
+        'no-await-in-loop': 'off',
+        'radix': 'off',
     },
     settings: {
         'import/resolver': [{

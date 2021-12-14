51ca8066698d1dad19dd5ec818575b02bc5eb4ab
AngaBlue <contact@anga.blue>
2021-12-14 15:05:30 +1100

    Updated rules

--- index.js
+++ index.js
@@ -20,27 +20,35 @@ module.exports = {
         'prettier',
     ],
     rules: {
-        'prettier/prettier': ['error', {
-            'printWidth': 160,
-            'tabWidth': 4,
-            'semi': true,
-            'singleQuote': true,
-            'trailingComma': 'none',
-            'arrowParens': 'avoid'
-        }, {
-                "usePrettierrc": false
-            }],
+        'prettier/prettier': [
+            'error',
+            {
+                printWidth: 140,
+                tabWidth: 4,
+                semi: true,
+                singleQuote: true,
+                trailingComma: 'none',
+                arrowParens: 'avoid',
+                endOfLine: 'lf'
+            }, {
+                usePrettierrc: false
+            }
+        ],
         'no-console': 'off',
         semi: ['error', 'always'],
         quotes: [
             'error',
             'single',
         ],
-        indent: ['error', 4, { 'SwitchCase': 1 }],
+        indent: ['error', 4, { SwitchCase: 1 }],
         'no-plusplus': 'off',
-        'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.tsx', '.ts'] }],
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx', '.ts'] }],
         'no-await-in-loop': 'off',
-        'radix': 'off',
+        radix: 'off',
+        'import/no-cycle': 'off',
+        'import/no-mutable-exports': 'off',
+        'no-underscore-dangle': 'off',
+        'no-bitwise': 'off'
     },
     /* TypeScript Configuration */
     overrides: [{
